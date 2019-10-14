3b5a5acdce4301abba40881ebaca85cffc26da0a
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-14 15:20:33 +0530

    yes

--- Scrap.java
+++ Scrap.java
@@ -27,6 +27,16 @@ public class Scrap {
         }
         in.close();
     }
+
+    public static void zeroMover() {
+        Scanner in = new Scanner(System.in);
+        int a[] = new int[in.nextInt()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        
+        in.close();
+    }
     public static void main(String[] args) {
         dups();
     }
