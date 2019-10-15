49fa065d22c917c754388f3379b3f682a8b0ac8a
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-15 08:20:32 +0530

    asd

--- StudentDetails.java
+++ StudentDetails.java
@@ -5,7 +5,7 @@ public class StudentDetails {
 	String name;
 	int roll_no;
 	double[] subject;
-	int marksObtained;
+	double marksObtained;
 	static double maxMarks = 400;
 	
 	public StudentDetails() {}
@@ -20,23 +20,48 @@ public class StudentDetails {
 		for(double m:subject) {
 			marksObtained += m;
 		}
+		System.out.println(marksObtained);
 	}
 	
 	void calculateMarks(int m) {
 		calculateMarks();
+		marksObtained  = marksObtained*(1.08);
+		if(marksObtained>400.0) {
+			marksObtained = 400.0;
+		}
+		System.out.println(marksObtained);
 	}
 	
 	void calculateMarks(double m) {
-		
+		calculateMarks();
+		marksObtained  = marksObtained*(1.045);
+		if(marksObtained>400.0) {
+			marksObtained = 400.0;
+		}
+		System.out.println(marksObtained);
 	}
 	
 	void calculateMarks(int m1, int m2) {
-		
+		calculateMarks();
+		marksObtained = marksObtained*(1.045 + 1.08);
+		if(marksObtained>400.0) {
+			marksObtained = 400.0;
+		}
+		System.out.println(marksObtained);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Scanner in = new Scanner(System.in);
+		String a = in.next();
+		int b = in.nextInt();
+		double c[] = new double[4];
+		for(int i = 0; i < 4; i++) {
+			c[i] = in.nextDouble();
+		}
+		StudentDetails s = new StudentDetails(a, b, c);
+		s.calculateMarks();
+		s.calculateMarks(2.0);
+		s.calculateMarks(1);
+		s.calculateMarks(1, 2);
 	}
-
 }
