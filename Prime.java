facf72cda16af65ec97d00c63b8f877ba0430239
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-22 11:36:38 +0530

    gsgf

--- Prime.java
+++ Prime.java
@@ -10,7 +10,7 @@ public class Prime {
         if(n == 2) {return true;}
         ArrayList<Integer> factors = new ArrayList<Integer>();
         factors.add(1);
-        for (int i = 2; i <=  n/2 +1; i++) {
+        for (int i = 2; i <=  n/2; i++) {
             if(n%i == 0) {
                 factors.add(i);
             }
